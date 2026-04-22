e349889b3219ad166d6bd7dc58119c6baa219a2e
Swilam20 <mahmoud.swilam.2020@gmail.com>
2026-04-22 02:01:30 +0200

    Create CartItemRepository.java

---
+++ food-delivery-app/src/main/java/com/mentorship/food_delivery_app/cart/repository/CartItemRepository.java
@@ -0,0 +1,35 @@
+package com.mentorship.ecommerce_app.cart.repository;
+
+import com.mentorship.ecommerce_app.cart.dto.CartItemRequest;
+import com.mentorship.ecommerce_app.cart.dto.CartItemView;
+import com.mentorship.ecommerce_app.cart.entity.CartItem;
+import com.mentorship.ecommerce_app.cart.entity.CartItemId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, CartItemId> {
+
+
+    List<CartItem> findByIdCartItemCartId(UUID cartId);
+
+    void deleteByIdCartItemCartId(UUID cartId);
+
+
+    @Query("""
+        SELECT new com.mentorship.ecommerce_app.cart.dto.CartItemView(
+            m.menuItemName,
+            ci.cartItemQuantity,
+            m.menuItemDescription,
+            m.menuItemPrice
+        )
+        FROM CartItem ci
+        JOIN ci.menuItem m
+        WHERE ci.id.cartItemCartId = :cartId
+    """)
+    List<CartItemView> findCartItemViews(UUID cartId);
+}
