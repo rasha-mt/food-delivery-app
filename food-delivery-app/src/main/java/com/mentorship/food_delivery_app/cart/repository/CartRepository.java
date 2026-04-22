fcac030770a30ab073f91ddf7455370097aa38b8
Swilam20 <mahmoud.swilam.2020@gmail.com>
2026-04-22 02:01:54 +0200

    Create CartRepository.java

---
+++ food-delivery-app/src/main/java/com/mentorship/food_delivery_app/cart/repository/CartRepository.java
@@ -0,0 +1,15 @@
+package com.mentorship.ecommerce_app.cart.repository;
+
+import com.mentorship.ecommerce_app.cart.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, UUID> {
+
+
+    Optional<Cart> findByCartCustomerId(UUID cartCustomerId);
+}
