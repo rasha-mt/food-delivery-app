42a3c64ae88598d35ae2cfd0ce481a366e33f4b0
rasha-mt <rasha.matar.2022@gmail.com>
2026-05-03 17:08:48 +0300

    Revert "remove h2 database and it's config"
    This reverts commit 22be1b481ff8a42314bbbdd925e14a83e228ba8a.

---
+++ food-delivery-app/src/test/java/com/mentorship/food_delivery_app/config/H2Functions.java
@@ -0,0 +1,10 @@
+package com.mentorship.food_delivery_app.config;
+
+import java.util.UUID;
+
+public class H2Functions {
+
+    public static String uuidv7() {
+        return UUID.randomUUID().toString();
+    }
+}
