7b9b12ea913a8b9d87496fec88da3d0b8c1115e3
rasha-mt <rasha.matar.2022@gmail.com>
2026-05-12 00:12:13 +0300

    add JWT filter and service

--- food-delivery-app/src/main/java/com/mentorship/food_delivery_app/common/secuirty/jwt/JwtService.java
+++ food-delivery-app/src/main/java/com/mentorship/food_delivery_app/common/secuirty/jwt/JwtService.java
@@ -1,4 +1,61 @@
 package com.mentorship.food_delivery_app.common.secuirty.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Date;
+import java.util.UUID;
+
+@Service
 public class JwtService {
+
+    private final String SECRET = "secret-key";
+    @Value("${JWT_SECRET}")
+    private String jwtSecret;
+
+    private SecretKey getKey() {
+        return Keys.hmacShaKeyFor(Base64.getDecoder().decode(jwtSecret));
+    }
+
+    public String generateToken(UUID customerId) {
+        return Jwts.builder()
+                .setSubject(String.valueOf(customerId))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 3600000))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public UUID  extractCustomerId(String token) {
+        String subject = Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+        return UUID.fromString(subject);
+    }
+
+    // ✅ VALIDATION METHOD
+    public boolean validate(String token) {
+        try {
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(getKey())
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            return claims.getExpiration().after(new Date());
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
 }
