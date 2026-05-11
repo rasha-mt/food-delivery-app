e76cbfdc457d786ae69263053033ef2eb09f3b49
rasha-mt <rasha.matar.2022@gmail.com>
2026-05-12 00:13:23 +0300

    generalize response

--- food-delivery-app/src/main/java/com/mentorship/food_delivery_app/common/responses/ApiResponse.java
+++ food-delivery-app/src/main/java/com/mentorship/food_delivery_app/common/responses/ApiResponse.java
@@ -1,4 +1,15 @@
 package com.mentorship.food_delivery_app.common.responses;
 
-public class ApiResponse {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiResponse<T> {
+    private Integer status;
+    private T data;
+    private String message;
+
 }
