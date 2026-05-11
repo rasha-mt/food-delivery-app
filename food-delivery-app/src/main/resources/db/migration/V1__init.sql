3fd3b4ee1d4d0f22187bcc4615c879f27173aebc
rasha-mt <rasha.matar.2022@gmail.com>
2026-05-12 00:10:09 +0300

    edit database schema

--- food-delivery-app/src/main/resources/db/migration/V1__init.sql
+++ food-delivery-app/src/main/resources/db/migration/V1__init.sql
@@ -155,7 +155,7 @@ CREATE TABLE IF NOT EXISTS cart
     cart_customer_id     UUID NOT NULL, -- REFERENCES customer(customer_id)
     is_locked            BIT              DEFAULT CAST(0 AS BIT),
     cart_current_rest_id UUID    ,       --REFERENCES restaurant_branch(branch_id)
-    cart_total           DECIMAL(7, 2)
+    cart_total           DECIMAL(7, 2) DEFAULT 0
 );
 CREATE TABLE IF NOT EXISTS cart_item
 (   cart_item_id  UUID PRIMARY KEY DEFAULT uuidv7(),
